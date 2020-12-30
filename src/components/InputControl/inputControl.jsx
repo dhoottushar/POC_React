5a592cac6913a1976a14ac28705b6bcd13fa12c4
arun0989 <arunkumarmundrathi@gmail.com>
2020-12-30 14:05:53 +0530

    Added sorting table

--- src/components/InputControl/inputControl.jsx
+++ src/components/InputControl/inputControl.jsx
@@ -1,17 +1,198 @@
-import React, { Component } from "react";
 
-export default class InputControl extends Component {
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
 
-        return (
-            <div className="home-buttons">
-                Table
-            </div>
+import React from "react";
 
-        );
-    }
-}
+import CRUDTable, {
+  Fields,
+  Field,
+  CreateForm,
+  UpdateForm,
+  DeleteForm,
+  Pagination
+} from "react-crud-table";
+
+// Component's Base CSS
+import "./inputControl.css";
+
+const DescriptionRenderer = ({ field }) => <textarea {...field} />;
+
+let tasks = [
+  {
+    id: 1,
+    title: "Create an example",
+    description: "Create an example of how to use the component"
+  },
+  {
+    id: 2,
+    title: "Improve",
+    description: "Improve the component!"
+  },
+  {
+    id: 3,
+    title: "Create a pagination example",
+    description: "Yeah!! It will be created."
+  },
+  {
+    id: 4,
+    title: "Sing a song",
+    description: "La, la, laaaa"
+  },
+  {
+    id: 5,
+    title: "Write something",
+    description: "Something"
+  }
+];
+
+const SORTERS = {
+  NUMBER_ASCENDING: mapper => (a, b) => mapper(a) - mapper(b),
+  NUMBER_DESCENDING: mapper => (a, b) => mapper(b) - mapper(a),
+  STRING_ASCENDING: mapper => (a, b) => mapper(a).localeCompare(mapper(b)),
+  STRING_DESCENDING: mapper => (a, b) => mapper(b).localeCompare(mapper(a))
+};
+
+const getSorter = data => {
+  const mapper = x => x[data.field];
+  let sorter = SORTERS.STRING_ASCENDING(mapper);
+
+  if (data.field === "id") {
+    sorter =
+      data.direction === "ascending"
+        ? SORTERS.NUMBER_ASCENDING(mapper)
+        : SORTERS.NUMBER_DESCENDING(mapper);
+  } else {
+    sorter =
+      data.direction === "ascending"
+        ? SORTERS.STRING_ASCENDING(mapper)
+        : SORTERS.STRING_DESCENDING(mapper);
+  }
+
+  return sorter;
+};
+
+let count = tasks.length;
+const service = {
+//   fetchItems: payload => {
+//     let result = Array.from(tasks);
+//     result = result.sort(getSorter(payload.sort));
+//     return Promise.resolve(result);
+//   },
+fetchItems: payload => {
+    const { activePage, itemsPerPage } = payload.pagination;
+    const start = (activePage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    let result = Array.from(tasks);
+    result = result.sort(getSorter(payload.sort));
+    return Promise.resolve(result.slice(start, end));
+  },
+  fetchTotal: payload => {
+    return Promise.resolve(tasks.length);
+  },
+  create: task => {
+    count += 1;
+    tasks.push({
+      ...task,
+      id: count
+    });
+    return Promise.resolve(task);
+  },
+  update: data => {
+    const task = tasks.find(t => t.id === data.id);
+    task.title = data.title;
+    task.description = data.description;
+    return Promise.resolve(task);
+  },
+  delete: data => {
+    const task = tasks.find(t => t.id === data.id);
+    tasks = tasks.filter(t => t.id !== task.id);
+    return Promise.resolve(task);
+  }
+};
+
+const styles = {
+  container: { margin: "auto", width: "fit-content" }
+};
+
+const InputControl = () => (
+  <div style={styles.container}>
+    <CRUDTable
+      caption="Tasks"
+      fetchItems={payload => service.fetchItems(payload)}
+    >
+      <Fields>
+        <Field name="id" label="Id" hideInCreateForm />
+        <Field name="title" label="Title" placeholder="Title" />
+        <Field
+          name="description"
+          label="Description"
+          render={DescriptionRenderer}
+        />
+      </Fields>
+      <CreateForm
+        title="Task Creation"
+        message="Create a new task!"
+        trigger="Create Task"
+        onSubmit={task => service.create(task)}
+        submitText="Create"
+        validate={values => {
+          const errors = {};
+          if (!values.title) {
+            errors.title = "Please, provide task's title";
+          }
+
+          if (!values.description) {
+            errors.description = "Please, provide task's description";
+          }
+
+          return errors;
+        }}
+      />
+
+      <UpdateForm
+        title="Task Update Process"
+        message="Update task"
+        trigger="Update"
+        onSubmit={task => service.update(task)}
+        submitText="Update"
+        validate={values => {
+          const errors = {};
+
+          if (!values.id) {
+            errors.id = "Please, provide id";
+          }
+
+          if (!values.title) {
+            errors.title = "Please, provide task's title";
+          }
+
+          if (!values.description) {
+            errors.description = "Please, provide task's description";
+          }
+
+          return errors;
+        }}
+      />
+
+      <DeleteForm
+        title="Task Delete Process"
+        message="Are you sure you want to delete the task?"
+        trigger="Delete"
+        onSubmit={task => service.delete(task)}
+        submitText="Delete"
+        validate={values => {
+          const errors = {};
+          if (!values.id) {
+            errors.id = "Please, provide id";
+          }
+          return errors;
+        }}
+      />
+       <Pagination
+        itemsPerPage={2}
+        fetchTotalOfItems={payload => service.fetchTotal(payload)}
+      />
+    </CRUDTable>
+  </div>
+);
+
+export default InputControl;
